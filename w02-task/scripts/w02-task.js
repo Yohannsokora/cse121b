bffb9958bb597061cc6e22c6efc5761b8dda5003
Yohannsokora <sok22002@byui.edu>
2024-03-11 08:46:48 -0600

    W2_task

---
+++ w02-task/scripts/w02-task.js
@@ -0,0 +1,40 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = 'Sokora Franck Yohann Sokora';
+let currentYear = 2024;
+let profilePicture = 'images/myImage.jpeg';
+
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.getElementById('img');
+
+
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong> ${fullName} </strong>`;
+yearElement.textContent = `${currentYear}`;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
+
+
+
+/* Step 5 - Array */
+let foods = ['Attieke', 'Fried Fish', 'Alloco'];
+foodElement.innerHTML += `<br> ${foods}`;
+let additionalFoodElement = ['Grilled cheese with tomato soup'];
+foods.push(additionalFoodElement);
+foodElement.innerHTML += `<br> ${additionalFoodElement}`;
+foods.shift();
+foodElement.innerHTML += `<br>${foods}`;
+foods.pop();
+foodElement.innerHTML += `<br>${foods}`;
+
+
+
+
